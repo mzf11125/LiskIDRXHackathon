669f6bcde439e74913ee2681f5cf8280dfbbf86e
mzf11125 <77999453+mzf11125@users.noreply.github.com>
2025-06-02 21:40:30 +0700

    tailwind integration

---
+++ dingdongrefactor/src/app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page of the DingDong app.</p>
+      <p>Here you can find information about the app and its features.</p>
+      <p>
+        Feel free to explore and learn more about what DingDong has to offer!
+      </p>
+    </div>
+  );
+}
